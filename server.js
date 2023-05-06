9c9fba61ce2ea003b9704c53459cb84c8bd8d28e
tiru-2001 <tirumaleshae709@gmail.com>
2023-05-06 19:26:58 +0530

    updated

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(morgan("dev"));
 
 app.use("/api/v1", router);
 app.use("/api/v1", blogrouter);
-// app.use(express.static(path.join(__dirname, "./blog/build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./blog/build/index.html"));
-// });
+app.use(express.static(path.join(__dirname, "./blog/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./blog/build/index.html"));
+});
 app.get("/", (req, res) => {
   res.send("hi");
 });
